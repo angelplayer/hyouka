46b8cc93aae2cab953d0fbfe0b3a7f7b74de77d3
angelplayer <angel.player@outlook.com>
2018-08-16 19:44:35 +0700

    Change

--- Feature/User/Login.cs
+++ Feature/User/Login.cs
@@ -1,16 +1,16 @@
-namespace hyouka_api.Feature
-{
+// namespace hyouka_api.Feature
+// {
 
-  public class LoginData
-  {
-    public string Username { get; set; }
-    public string Password { get; set; }
+//   public class LoginData
+//   {
+//     public string Username { get; set; }
+//     public string Password { get; set; }
 
 
-  }
+//   }
 
-  public class Command : IRequest<UserEnvelope>
-  {
+//   public class Command : IRequest<UserEnvelope>
+//   {
 
-  }
-}
+//   }
+// }
--- Feature/User/UserController.cs
+++ Feature/User/UserController.cs
@@ -1,22 +1,22 @@
 
-using MediatR;
-using Microsoft.AspNetCore.Mvc;
+// using MediatR;
+// using Microsoft.AspNetCore.Mvc;
 
-namespace hyouka_api
-{
-  public class UserController : Controller
-  {
-    private IMediator mediator;
+// namespace hyouka_api
+// {
+//   public class UserController : Controller
+//   {
+//     private IMediator mediator;
 
-    public UserController(IMediator mediator)
-    {
-      this.mediator = mediator;
-    }
+//     public UserController(IMediator mediator)
+//     {
+//       this.mediator = mediator;
+//     }
 
-    [HttpGet("Login")]
-    public async Task<UserEnvelope> Login([FromBody] Login.Command command)
-    {
+//     [HttpGet("Login")]
+//     public async Task<UserEnvelope> Login([FromBody] Login.Command command)
+//     {
 
-    }
-  }
-}
+//     }
+//   }
+// }
